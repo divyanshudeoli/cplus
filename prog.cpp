c4f7db7fe469e09d9194cd64be5bc80674e068d7
Divyanshu Deoli <60615629+divyanshudeoli@users.noreply.github.com>
2020-03-15 21:14:32 +0530

    function

--- prog.cpp
+++ prog.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
+void print()
+{
+  cout<<"Is it going to run";
+}
 void main()
 {
-cout<<"Hello World";}
+cout<<"Hello World\n";
+Cout<<"Working on Github";  
+print();
+}
